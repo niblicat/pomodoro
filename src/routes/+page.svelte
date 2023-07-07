45e2616f7756ed0e5cacdcbf8292b15d0f8cd479
niblicat <maybenaebli@gmail.com>
2023-07-06 21:47:29 -0500

    got some text on the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<body>
+    <div class="timer">
+        <h2>Timer</h2>
+    </div>
+</body>
+
+<style>
+    .timer {
+        justify-content: center;
+        text-align: center;
+        
+    }
+</style>
