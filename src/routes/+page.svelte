07b2876a7f5174c2948e6f50f76a0d251bb8da54
niblicat <maybenaebli@gmail.com>
2023-07-06 21:54:20 -0500

    added new stuf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+    import type { ActionData, PageData, SubmitFunction } from './$types'
 
+    export let data: PageData
+    export let form: ActionData
+
+    let loading = false;
+    const actionLoad: SubmitFunction = (input) => {
+        console.log('in progress');
+        loading = true;
+        return async ({ update }) => {
+            await update();
+            loading = false;
+        }
+
+    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -8,6 +22,18 @@
 <body>
     <div class="timer">
         <h2>Timer</h2>
+        <p class="numbersTime">13:45<p>
+        <div class="buttons">
+            <form
+            method="POST"
+            action=?/startTime
+            >
+                <button
+                >
+                    B
+                </button>
+            </form>
+        </div>
     </div>
 </body>
 
@@ -15,6 +41,8 @@
     .timer {
         justify-content: center;
         text-align: center;
-        
+    }
+    .timer .buttons form {
+        display:inline;
     }
 </style>
