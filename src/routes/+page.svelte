bfdb70fd18653b937b3f707c895ba0648837d785
niblicat <maybenaebli@gmail.com>
2023-07-10 17:49:47 -0500

    prettied up presentation, fixed loading cursor pos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,13 +55,12 @@
 
     // moves loading spinner to cursor's position
     function loadingElementToCursor() {
+        console.log('move');
         if (mouseHasMoved > 1) {
-            loadingIcon.style.top = m.y - 20 + 'px';
-            loadingIcon.style.left = m.x - 20 + 'px';
-        }
-        else if (mouseHasMoved === 1) {
-            loadingIcon.style.margin = '0px';
+            loadingIcon.style.top = m.y - 15 + 'px';
+            loadingIcon.style.left = m.x - 15 + 'px';
             loadingIcon.style.position = "absolute";  
+            loadingIcon.style.margin = '0px';
         }
         mouseHasMoved++;
     }
@@ -81,8 +80,9 @@
 <html lang="en">
 <body>
 <div class="background">
+    <div class="wrapper center">
     <div class="timer center">
-        <h2>Timer</h2>
+        <div class="timerTitle">Timer</div>
         <p class="numbersTime fade" transition:fade>
             {#each $timeElement as e (e.type)}
                 {#if !((e.type === 'hours') && (e.value <= 0))}
@@ -90,27 +90,25 @@
                 {/if}
             {/each}
         </p>
-        <div class="buttons center">
-            <button
-            class="fade"
-            on:click={timer.timerState === timer.TimerStates.Pomodoro ? timer.pomodoroActive : timer.startTimer}
-            title="Start"
-            id="start"
-            >
-            
-            Start
-            </button>
-            <button
-            class="fade"
-            on:click={timer.stopTimer}
-            title="Pause"
-            id="pause"
-            >
-                Pause
-            </button>
-        </div>
+        <button
+        class="fade"
+        on:click={timer.timerState === timer.TimerStates.Pomodoro ? timer.pomodoroActive : timer.startTimer}
+        title="Start"
+        id="start"
+        >
+        
+        start
+        </button>
+        <button
+        class="fade"
+        on:click={timer.stopTimer}
+        title="Pause"
+        id="pause"
+        >
+            pause
+        </button>
     </div>
-
+    </div>
     <div 
     id={loading === true ? "loadingIcon" : "noID"}
     class="center"
@@ -119,6 +117,7 @@
     {#if debug}
         <p>{m.x}, {m.y}</p>
     {/if}
+
 </div>
 </body>
 </html>
@@ -129,60 +128,118 @@
         margin: 0px;
         padding: 0px;
     }
+
     body {
         height: 100vh;
         width: 100vw;
     }
+
     * {
         box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
     }
+
     button {
-        border-radius: 20%;
+        border-radius: 25px;
         min-width: 60px;
         border: 2px solid #14FF63;
         background-color: white;
         font-size: 20px;
     }
+
     button:hover {
         background-color: #1499FF;
         border: 2px solid white;
-        transform: scale(1.2);
+        transform: scale(1.1);
     }
+
     button:active {
-        transform: scale(0.5);
+        transform: scale(0.9);
     }
+
     .center {
+        display: flex;
         justify-content: center;
+        align-items: center;
         text-align: center;
     }
+
     .background {
         height: 100%;
         width: 100%;
         padding: 10px;
     }
+
+    .wrapper {
+        height: 80%;
+        width: 100%;
+    }
+
     .timer {
-        max-width: 20%;
+        width: 80%;
+        max-width: 380px;
+        height: 100%;
+        max-height: 235px;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 30% 30% 30% 10%;
+        grid-gap: 10px;
+        border-radius: 25px;
+        background-color: #13E896;
         margin: auto;
         font-size: 20px;
     }
-    .timer .buttons {
-        display: grid;
-        justify-content: space-around;
-        grid-template: 40px / minmax(80px, 100px) minmax(80px, 100px);
+
+    .timer > * {
+        display: flex;
+        justify-self: center;
+        align-self: center;
+        justify-content: center;
+        align-items: center;
     }
+
+    .timer .timerTitle {
+        font-size: 25px;
+        grid-row: 1;
+        grid-column: span 2;
+    }
+
+    .timer .numbersTime {
+        font-size: 48px;
+        grid-row: 2;
+        grid-column: span 2;
+    }
+
+    .timer button {
+        grid-row: 3;
+        display: grid;
+        min-width: 60px;
+        width: 80%;
+    }
+
+    .timer button#start {
+    grid-column: 1;
+    }
+
+    .timer button#pause {
+    grid-column: 2;
+    }
+
     #loadingIcon {
         margin: 8px calc(50vw - 30px) 8px calc(50vw - 30px);
-        border: 4px solid #f3f3f3;
-        border-top: 4px solid #3073BA;
+        border: 4px solid white;
+        border-top: 4px solid #13C4E8;
         border-radius: 50%;
         width: 30px;
         height: 30px;
         animation: spin 0.5s linear infinite;
     }
+
     @keyframes spin {
         0% { transform: rotate(0deg); }
         100% { transform: rotate(360deg); }
     }
+
     .fade {
         transition: all .2s ease-in-out;
         animation: fadeIn .5s;
