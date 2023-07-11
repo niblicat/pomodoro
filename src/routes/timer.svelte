a01444e5388abc1236e2293eca1ff328e682d9db
niblicat <maybenaebli@gmail.com>
2023-07-11 15:04:24 -0500

    removed some console log outputs

--- src/routes/timer.svelte
+++ src/routes/timer.svelte
@@ -112,14 +112,11 @@
 		return new Promise(async (resolve) => {
 			let timeDifference: number = 0;
 			currentTime = Date.now();
-            console.log('goaltime: ' + goalTime);
 			endTime = currentTime + 100 * goalTime;
             let timerProgressState: boolean = await TimerProgress.getTimerInProgress();
             timeDifference = endTime - currentTime;
-            console.log('time difference: ' + timeDifference)
             interval = setInterval(async () => {
                 await formatTime(timeDifference / 100);
-                console.log(timeDifference / 100);
                 timerProgressState = await TimerProgress.getTimerInProgress();
                 currentTime = Date.now();
                 timeDifference = endTime - currentTime;
@@ -155,7 +152,6 @@
 	// changes the goal time from current and end time values
 	async function updateGoalTime() {
         goalTime = (endTime - currentTime) / 100;
-        console.log('goalTime in update: ' + goalTime);
 	}
 
 	// pauses the timer and stops the time updater from looping
